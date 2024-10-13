46e27afc5628f1289d811fbf98f68cc3e44af9d7
Karol Ziaja <kziaja34@gmail.com>
2024-10-13 14:32:16 +0200

    asynchronous reading changed into threads

--- BlazorApp1/BlazorApp1/obj/Release/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Release/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c675f1a06589a5725653a4e73e352e8fca57050")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97822ef35e69612cb8d47431bcb82d3775b8633")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
