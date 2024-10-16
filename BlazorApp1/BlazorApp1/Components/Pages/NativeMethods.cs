7d99b4c2dc71ee338d8196009dc67a1fc629a768
Karol Ziaja <kziaja34@gmail.com>
2024-10-17 00:09:21 +0200

    smth

--- BlazorApp1/BlazorApp1/Components/Pages/NativeMethods.cs
+++ BlazorApp1/BlazorApp1/Components/Pages/NativeMethods.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 
 public static class NativeMethods
 {
-	[DllImport("../x64/Release/JAAsm.dll", CallingConvention = CallingConvention.Cdecl)]
+	[DllImport("../x64/Debug/JAAsm.dll", CallingConvention = CallingConvention.Cdecl)]
 	public static extern int MyAdd(int a, int b);
 
     //[DllImport("../x64/Debug/JAC.dll", CallingConvention = CallingConvention.Cdecl)]
     //public static extern IntPtr ApplyFIRFilterFromDLL(byte[] audioData, int dataLength, IntPtr outputLength);
 
-    [DllImport("../x64/Release/JAC.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("../x64/Debug/JAC.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern int MyMinus(int a, int b);
 }
--- BlazorApp1/JAAsm/JAAsm.asm
+++ BlazorApp1/JAAsm/JAAsm.asm
@@ -1,25 +1,4 @@
-;-------------------------------------------------------------------------
-;.586
-;INCLUDE C:\masm32\include\windows.inc 
-
 .CODE
-;-------------------------------------------------------------------------
-; To jest przyk³adowa funkcja. 
-;-------------------------------------------------------------------------
-;parametry funkcji: RCX RDX R8 R9 stos, 
-;lub zmiennoprzec.  XMM0 1 2 3
-MyProc1 proc		
-add 	rcx, rdx
-mov 	rax, rcx
-jnc ET1
-ror	rcx,1
-mul 	rcx
-ret
-ET1:	
-neg 	rax
-ret
-MyProc1 endp
-
 ;-------------------------------------------------------------------------
 ; This is a sample function that takes two integer arguments and returns their sum.
 ;-------------------------------------------------------------------------
