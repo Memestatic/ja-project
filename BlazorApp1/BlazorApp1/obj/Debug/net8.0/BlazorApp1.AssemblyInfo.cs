02e446f1c364c4bc2a5385ca95420f5f3c35d2e9
Karol Ziaja <kziaja34@gmail.com>
2024-10-29 00:06:53 +0100

    added spliting data simulation

--- BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1f0e021571c5a1450fd6cd5dbe91bbcee5fe5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce592703b28c7697abb09c06141662f7c17e849a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
