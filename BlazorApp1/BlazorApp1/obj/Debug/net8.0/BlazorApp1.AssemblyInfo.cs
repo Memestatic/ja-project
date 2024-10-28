ce592703b28c7697abb09c06141662f7c17e849a
Karol Ziaja <kziaja34@gmail.com>
2024-10-28 18:33:44 +0100

    data splitting optimized

--- BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb5d0880aeac0688e09b3a1f5805a48b68be26d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1f0e021571c5a1450fd6cd5dbe91bbcee5fe5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
