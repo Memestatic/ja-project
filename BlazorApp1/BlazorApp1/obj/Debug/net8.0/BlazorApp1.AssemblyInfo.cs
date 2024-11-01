826acde7c7952036dd310d40db0f959028ea8b3e
Karol Ziaja <kziaja34@gmail.com>
2024-11-01 20:47:19 +0100

    compiled

--- BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aeb59001f7cdbfb863f7fac5a838a4fb402951a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251762d00c86a49627654afdd4f276f041b1c967")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
