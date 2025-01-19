d8ed1b0fbc4d3fde6c6ac507a2609b1f13a1a2fb
Karol Ziaja <kziaja34@gmail.com>
2025-01-19 16:52:11 +0100

    updated comments

--- BlazorApp1/JAAsm/JAAsm.asm
+++ BlazorApp1/JAAsm/JAAsm.asm
@@ -1,93 +1,92 @@
 .code
 
+; Definicja procedury ProcessFIRFilter
 ProcessFIRFilter proc
-    push rbp
-    mov rbp, rsp
-    jmp _start
+    push rbp                     ; Zachowanie wartoœci bazowego wskaŸnika stosu
+    mov rbp, rsp                 ; Ustawienie nowego bazowego wskaŸnika stosu
+    jmp _start                   ; Przejœcie do etykiety _start
 
 _start:
-    ; Arguments:
-    ; rcx = input array pointer
-    ; rdx = output array pointer
-    ; r8 = coefficients array pointer
-    ; r9 = outputLength
-    ; [rsp + 48] = coefficientsLength
+    ; Opis argumentów przekazywanych do procedury:
+    ; rcx = wskaŸnik do tablicy wejœciowej (input array)
+    ; rdx = wskaŸnik do tablicy wyjœciowej (output array)
+    ; r8  = wskaŸnik do tablicy wspó³czynników (coefficients array)
+    ; r9  = d³ugoœæ tablicy wyjœciowej (outputLength)
+    ; [rsp + 48] = d³ugoœæ tablicy wspó³czynników (coefficientsLength)
+    ; r10 = d³ugoœæ tablicy wspó³czynników (przechowywana w r10 dla wydajnoœci)
 
-    ; r10 = coefficientsLength
-    mov r10, [rsp + 48]  
-    
+    mov r10, [rsp + 48]          ; Pobranie d³ugoœci tablicy wspó³czynników z stosu
+
     mov rax, r10
-    dec rax
-    ; Outer loop
-    mov r11, 0      ; r11 is the index for the output array
+    dec rax                      ; rax = coefficientsLength - 1
+
+    ; Inicjalizacja indeksu dla tablicy wyjœciowej
+    mov r11, 0                   ; r11 = indeks aktualny dla output array
+
 loop_n:
-    cmp r11, r9     ; Compare current index with outputLength
-    jge end_loop_n      ; If index >= outputLength, end the loop
+    cmp r11, r9                  ; Porównanie bie¿¹cego indeksu z outputLength
+    jge end_loop_n               ; Jeœli indeks >= outputLength, zakoñcz pêtlê zewnêtrzn¹
 
-    ; Inner loop
-    mov r12, 0      ; r12 is the index for the coefficients array
+    ; Inicjalizacja indeksu dla tablicy wspó³czynników
+    mov r12, 0                   ; r12 = indeks aktualny dla coefficients array
 
-    cmp r11, rax
-    jl skip_coef
+    cmp r11, rax                 ; Sprawdzenie, czy r11 < coefficientsLength - 1
+    jl skip_coef                 ; Jeœli tak, przejdŸ do pomijania obliczeñ wspó³czynników
 
-    ; Clear ymm registers
-    vxorps ymm0, ymm0, ymm0
-    vxorps ymm1, ymm1, ymm1
-    vxorps ymm2, ymm2, ymm2
-    vxorps ymm3, ymm3, ymm3
-    vxorps ymm4, ymm4, ymm4
-    vxorps ymm5, ymm5, ymm5
-    vxorps ymm6, ymm6, ymm6
+    ; Czyszczenie rejestrów YMM, aby przygotowaæ je do akumulacji wyników
+    vxorps ymm0, ymm0, ymm0      ; Zerowanie ymm0
+    vxorps ymm4, ymm4, ymm4      ; Zerowanie ymm4
+    vxorps ymm5, ymm5, ymm5      ; Zerowanie ymm5
+    vxorps ymm6, ymm6, ymm6      ; Zerowanie ymm6
 
 loop_k:
-    cmp r12, r10        ; Compare current index with coefficientsLength
-    jge end_loop_k      ; If index >= coefficientsLength, end the inner loop
+    cmp r12, r10                 ; Porównanie indeksu wspó³czynników z coefficientsLength
+    jge end_loop_k               ; Jeœli indeks >= coefficientsLength, zakoñcz pêtlê wewnêtrzn¹
 
-    mov r13, r11
-    sub r13, r12
-    shl r13, 2      ; Multiply by 4 to get the correct index
-    cmp r13, 0
-    jl end_loop_k       ; If r13 < 0, skip this iteration
+    mov r13, r11                 ; Kopiowanie bie¿¹cego indeksu wyjœciowego do r13
+    sub r13, r12                 ; Obliczenie odpowiedniego indeksu wejœciowego
+    shl r13, 2                   ; Przemno¿enie indeksu przez 4 (rozmiar float) dla obliczeñ adresów
 
+    cmp r13, 0                   ; Sprawdzenie, czy obliczony indeks jest nieujemny
+    jl end_loop_k                ; Jeœli indeks < 0, pomiñ tê iteracjê
 
-    sub r13, 28
-    vmovups ymm5, YMMWORD PTR [rcx + r13]
-    vmovups ymm6, YMMWORD PTR [r8 + r12 * 4]
+    sub r13, 28                  ; Korekta indeksu wejœciowego (offset)
+    
+    ; £adowanie danych wejœciowych i wspó³czynników do rejestrów YMM
+    vmovups ymm5, YMMWORD PTR [rcx + r13]      ; £adowanie 8 floatów z input array
+    vmovups ymm6, YMMWORD PTR [r8 + r12 * 4]   ; £adowanie 8 floatów z coefficients array
 
-    add r12, 8
-    jmp multiply_and_add
+    add r12, 8                   ; Zwiêkszenie indeksu wspó³czynników o 8 (przetwarzanie wektorowe)
+    jmp multiply_and_add         ; Przejœcie do etapu mno¿enia i akumulacji
 
 multiply_and_add:
-    ; Multiply and add to ymm4
-    vfmadd231ps ymm4, ymm5, ymm6        ; ymm4 += ymm5 * ymm6
-    jmp loop_k      ; Continue inner loop
+    ; Wykonanie operacji mno¿enia i dodania dla wektorów
+    vfmadd231ps ymm4, ymm5, ymm6  ; ymm4 += ymm5 * ymm6
+    jmp loop_k                    ; Kontynuacja pêtli wewnêtrznej
 
 end_loop_k:
+    ; Redukcja YMM4 do pojedynczej wartoœci float
+    vxorps xmm0, xmm0, xmm0                  ; Zerowanie rejestru xmm0
+    vextractf128 xmm0, ymm4, 1                ; Wyodrêbnienie górnej czêœci YMM4 do xmm0
+    vaddps xmm4, xmm4, xmm0                   ; Dodanie górnej czêœci do dolnej
+    vhaddps xmm4, xmm4, xmm4                  ; Suma horyzontalna 8 floatów do 4
+    vhaddps xmm4, xmm4, xmm4                  ; Suma horyzontalna 4 floatów do 2
+    vmovss DWORD PTR [rdx + r11 * 4], xmm4    ; Przechowanie wyniku jako pojedynczego float w output array
 
-    vxorps xmm0, xmm0, xmm0
-
-    vextractf128 xmm0, ymm4, 1      ; Extract the upper part of ymm0
-    vaddps xmm4, xmm4, xmm0     ; Add the upper part to the lower part
-
-    vhaddps xmm4, xmm4, xmm4
-    vhaddps xmm4, xmm4, xmm4
-
-    vmovss DWORD PTR [rdx + r11 * 4], xmm4  ; Store the result from the lower part of ymm0
-
-    ; Increment output index and continue main loop
+    ; Inkrementacja indeksu wyjœciowego i kontynuacja pêtli zewnêtrznej
     inc r11
     jmp loop_n
 
 skip_coef:
-    ; Increment output index and continue main loop
-    inc r11
-    jmp loop_n
+    ; Je¿eli bie¿¹cy indeks wyjœciowy < coefficientsLength - 1, pomiñ obliczenia wspó³czynników
+    inc r11                     ; Inkrementacja indeksu wyjœciowego
+    jmp loop_n                  ; Kontynuacja pêtli zewnêtrznej
 
 end_loop_n:
-    ; End algorithm
-    mov rsp, rbp
-    pop rbp
-    ret
+    ; Zakoñczenie procedury FIR filter
+    mov rsp, rbp                ; Przywrócenie wskaŸnika stosu
+    pop rbp                     ; Przywrócenie bazowego wskaŸnika stosu
+    ret                         ; Powrót z procedury
 
 ProcessFIRFilter endp
 
